eb1f00cb65a7e6451303d223f9548f3230f83ce1
rushikeshs-cuelogic <rushikesh.shelar@cuelogic.com>
2017-08-02 16:54:52 +0530

    NODEJS projectStructure: Initial commit

---
+++ modules/user/server/route/user.server.routes.js
@@ -0,0 +1,10 @@
+import userController from "../controller/user.server.controller.js";
+import validator from "../validator/user.server.validator.js";
+
+module.exports = (routerobject) => {
+    
+    // route for createUser
+    routerobject.get("/createUser", validator.createUser, userController.createUser);
+
+    return routerobject;
+};

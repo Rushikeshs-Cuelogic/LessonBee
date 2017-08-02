0323d7192d9149f613194d2bb34376bc78fed548
deepalik-cuelogic <deepali.kakade@cuelogic.com>
2017-08-02 17:27:33 +0530

    AngularJs folders added

---
+++ modules/client.app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function() {
+    // Declare app level module
+    angular
+        .module('lessonBee', [
+            'ui.router',
+            'ngAnimate',
+            'base'
+            'user'
+        ])
